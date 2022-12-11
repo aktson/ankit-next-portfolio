e8a35baec92f4a69915284a69b62bd3cb5c5ffb9
Ankit Soni <akt_soni@yahoo.co.in>
2022-12-11 15:39:25 +0100

    favicon added

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Footer from '../components/layout/Footer'
 import Main from '../components/layout/Main'
 import Header from '../components/layout/Header'
+import Favicon from '../components/layout/Favicon'
 
 
 export default function Home() {
@@ -10,10 +11,10 @@ export default function Home() {
       <Head>
         <title>Ankit Soni Portfolio</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
         <meta name="author" content="Ankit Soni" />
         <meta name="description" content="Ankit Soni - Kreative profesjonell Front-end Utvikler/ Creative professional front-end developer" />
         <meta name="keywords" content="ankit, soni, Ankit Soni, oslo, web development, javascript, HTML, CSS3, reactJs, nextJs, norge, norway, web designer, web utvikler, front end developer, front end utvikler" />
+        <Favicon />
       </Head>
       <Header />
       <Main />
