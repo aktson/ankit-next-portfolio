4289f5ad83a825e5fd52cddf79f0947a4c4f0442
Ankit Soni <akt_soni@yahoo.co.in>
2022-12-11 15:41:47 +0100

    document updated

--- pages/index.js
+++ pages/index.js
@@ -8,14 +8,6 @@ import Favicon from '../components/layout/Favicon'
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Ankit Soni Portfolio</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="author" content="Ankit Soni" />
-        <meta name="description" content="Ankit Soni - Kreative profesjonell Front-end Utvikler/ Creative professional front-end developer" />
-        <meta name="keywords" content="ankit, soni, Ankit Soni, oslo, web development, javascript, HTML, CSS3, reactJs, nextJs, norge, norway, web designer, web utvikler, front end developer, front end utvikler" />
-        <Favicon />
-      </Head>
       <Header />
       <Main />
       <Footer />
