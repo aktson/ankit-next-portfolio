23d5228f617833e51ceea39e66bdab2f71910e8c
Ankit Soni <akt_soni@yahoo.co.in>
2022-12-16 12:00:45 +0100

    layout removed

--- components/sections/Hero.jsx
+++ components/sections/Hero.jsx
@@ -5,78 +5,45 @@ import LangContext from "../context/LangContext";
 import bgLight from "../../public/images/hero.svg";
 import bgDark from "../../public/images/hero-dark.svg";
 import { motion } from "framer-motion";
+import { wavyTextChild, wavyTextParent } from "../uicomponents/animationVariants/WavyText";
 
 function Hero() {
 	const { isEng, isChecked } = React.useContext(LangContext);
 
 	const heading1 = isEng ? "Hello! I'M" : "Hei! Jeg er";
 	const heading2 = "ANKIT SONI";
-	const heading3 = isEng ? "Front-end Developer & Web Designer" : "Front-end Utvikler & Web Designer";
+	const heading3 = isEng ? `Front-end Developer & Web Designer` : "Front-end Utvikler & Web Designer";
 
-	const parent = {
-		hidden: {
-			opacity: 0,
-		},
-		visible: (i = 1) => ({
-			opacity: 1,
-			transition: {
-				staggerChildren: 0.05,
-				delayChildren: i * 0.1,
-			},
-		}),
-	};
-
-	const child = {
-		visible: {
-			opacity: 1,
-			y: 0,
-			transition: {
-				type: "spring",
-				damping: 12,
-				stiffness: 200,
-			},
-		},
-		hidden: {
-			opacity: 0,
-			y: 20,
-			transition: {
-				type: "spring",
-				damping: 12,
-				stiffness: 200,
-			},
-		},
-	};
-
-	const head1 = heading1.split("").map((char, index) => {
+	const head1 = heading1.split("").map((letter, index) => {
 		return (
-			<motion.span key={char + "-" + index} variants={child} className="text-4xl">
-				{char}
+			<motion.span key={index} variants={wavyTextChild} className="text-3xl">
+				{letter}
 			</motion.span>
 		);
 	});
 
-	const head2 = heading2.split("").map((char, index) => {
+	const head2 = heading2.split("").map((letter, index) => {
 		return (
-			<motion.span key={char + "-" + index} variants={child} className="text-primary">
-				{char}
+			<motion.span key={index} variants={wavyTextChild} className="text-primary">
+				{letter}
 			</motion.span>
 		);
 	});
-	const head3 = heading3.split("").map((char, index) => {
+	const head3 = heading3.split("").map((letter, index) => {
 		return (
-			<motion.span key={char + "-" + index} variants={child} className="text-4xl">
-				{char}
+			<motion.span key={letter + "-" + index} variants={wavyTextChild} className="text-3xl">
+				{letter}
 			</motion.span>
 		);
 	});
 
 	return (
 		<section id="hero" className={` flex flex-col justify-center h-screen relative`}>
-			<Image src={isChecked ? bgDark : bgLight} alt="background vector wavees" layout="fill" />
+			<Image src={isChecked ? bgDark : bgLight} alt="background vector wavees" fill object-fit="cover" />
 			{/* <Header /> */}
 			<div className="container  mx-auto px-1 relative flex items-center justify-center text-center">
 				<div className="p-2 max-w-xl w-full">
-					<motion.h1 variants={parent} initial="hidden" animate="visible">
+					<motion.h1 variants={wavyTextParent} initial="hidden" animate="visible">
 						{head1}
 						<br />
 						{head2}
--- components/uicomponents/animationVariants/WavyText.js
+++ components/uicomponents/animationVariants/WavyText.js
@@ -1,58 +1,33 @@
-import { motion } from "framer-motion";
-
-
-const WavyText = ({ text, delay = 0, duration = 0.05, ...props }) => {
-    const letters = Array.from(text);
-
-    const parent = {
-        hidden: {
-            opacity: 0
+export const wavyTextParent = {
+    hidden: {
+        opacity: 0,
+    },
+    visible: {
+        opacity: 1,
+        transition: {
+            // staggerChildren: 0.05,
+            // delayChildren: 0.1,
         },
-        visible: (i = 1) => ({
-            opacity: 1,
-            transition: {
-                staggerChildren: duration,
-                delayChildren: i * delay
-            }
-        })
-    };
-
-    const child = {
-        visible: {
-            opacity: 1,
-            y: 0,
-            transition: {
-                type: "spring",
-                damping: 12,
-                stiffness: 200
-            }
-        },
-        hidden: {
-            opacity: 0,
-            y: 20,
-            transition: {
-                type: "spring",
-                damping: 12,
-                stiffness: 200
-            }
-        }
-    };
-
-    return (
-        <motion.h1
-            style={{ display: "flex", overflow: "hidden" }}
-            variants={parent}
-            initial="hidden"
-            animate={"visible"}
-            {...props}
-        >
-            {letters.map((letter, index) => (
-                <motion.span key={index} variants={child}>
-                    {letter === " " ? "\u00A0" : letter}
-                </motion.span>
-            ))}
-        </motion.h1>
-    );
+    }
 };
 
-export default WavyText;
+export const wavyTextChild = {
+    visible: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            type: "spring",
+            damping: 12,
+            stiffness: 200,
+        },
+    },
+    hidden: {
+        opacity: 0,
+        scale: 0.2,
+        transition: {
+            type: "spring",
+            damping: 12,
+            stiffness: 200,
+        },
+    },
+};
