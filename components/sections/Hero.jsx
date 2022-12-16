f7e4bec5151335a9c1ea310aee1f3e226c245666
Ankit Soni <akt_soni@yahoo.co.in>
2022-12-16 20:07:00 +0100

    Update Hero.jsx

--- components/sections/Hero.jsx
+++ components/sections/Hero.jsx
@@ -13,7 +13,8 @@ function Hero() {
 
 	const heading1 = isEng ? "Hello! I'M" : "Hei! Jeg er";
 	const heading2 = "ANKIT SONI";
-	const heading3 = isEng ? `Front-end Developer & Web Designer` : "Front-end Utvikler & Web Designer";
+	const heading3 = isEng ? "Front-end Developer" : "Front-end Utvikler ";
+	const heading4 = " & Web Designer";
 
 	return (
 		<section id="hero" className={` flex flex-col justify-center h-screen relative`}>
@@ -26,9 +27,10 @@ function Hero() {
 						initial="hidden"
 						animate="visible"
 						className="flex justify-center flex-col items-center leading-loose gap-5">
-						<HeroHeading heading={heading1} font="text-3xl" />
-						<HeroHeading heading={heading2} font="text-primary  text-6xl" />
-						<HeroHeading heading={heading3} font="text-3xl" />
+						<HeroHeading heading={heading1} font="text-3xl sm:text-4xl" />
+						<HeroHeading heading={heading2} font="text-primary  text-4xl sm:text-5xl" />
+						<HeroHeading heading={heading3} font="text-3xl sm:text-4xl" />
+						<HeroHeading heading={heading4} font="text-3xl sm:text-4xl" />
 					</motion.h1>
 					<Link className="btn btn-primary btn-md sm:btn-wide " to="about" smooth={true} duration={500}>
 						{isEng ? "See my work" : "Se mitt arbeid"}
