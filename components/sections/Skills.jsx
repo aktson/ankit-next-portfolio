edd88809d74dc752dfc0924ee1471d24a169cc41
Ankit Soni <akt_soni@yahoo.co.in>
2022-12-26 22:40:16 +0100

    min height added

--- components/sections/Skills.jsx
+++ components/sections/Skills.jsx
@@ -6,7 +6,7 @@ import Heading from "../uicomponents/animationVariants/Heading";
 function Skills(data) {
 	const { isEng } = React.useContext(LangContext);
 	return (
-		<section className=" py-16 sm:py-32 px-1 lg:h-screen  bg-base-200" id="skills">
+		<section className=" py-16 sm:py-32 px-1 min-h-screen  bg-base-200" id="skills">
 			<div className=" max-w-5xl  mx-auto flex flex-col justify-center container items-center">
 				<Heading heading={isEng ? "SKILLS" : "FERDIGHETER"} />
 				<SkillsTabs data={data} />
